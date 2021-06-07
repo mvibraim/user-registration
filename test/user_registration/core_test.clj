4cbaba83b170e4caa757b7858a1fcc9156d05d84
Marcus Vinícius Ibraim <mvibraim@gmail.com>
2021-06-07 14:04:34 -0300

    Project generated by lein new app command

---
+++ test/user_registration/core_test.clj
@@ -0,0 +1,7 @@
+(ns user-registration.core-test
+  (:require [clojure.test :refer :all]
+            [user-registration.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

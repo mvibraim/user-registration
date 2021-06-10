81cd802e9824df1a29750cf0c08af4c2d51089fc
Marcus Vinícius Ibraim <mvibraim@gmail.com>
2021-06-10 14:18:27 -0300

    Add tests for country pages

---
+++ test/user_registration/services/country-pages/brpages_test.clj
@@ -0,0 +1,22 @@
+(ns user-registration.services.country-pages.brpages-test
+  (:require [clojure.test :refer :all]
+            [user-registration.services.country-pages.brpages :refer :all]))
+
+(deftest list-ordered-pages-test
+  (testing "Should return 1 page"
+    (let [page-ordering [:document]
+          ordered-pages (:pages (listar-paginas page-ordering))]
+      (is (= 1 (count ordered-pages)))))
+
+  (testing "Should return 2 page"
+    (let [page-ordering [:document :name]
+          ordered-pages (:pages (listar-paginas page-ordering))]
+      (is (= 2 (count ordered-pages)))))
+  (testing "Should return 3 page"
+    (let [page-ordering [:document :name :date]
+          ordered-pages (:pages (listar-paginas page-ordering))]
+      (is (= 3 (count ordered-pages)))))
+  (testing "Should return 4 page"
+    (let [page-ordering [:document :name :date :email]
+          ordered-pages (:pages (listar-paginas page-ordering))]
+      (is (= 4 (count ordered-pages))))))
